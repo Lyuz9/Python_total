85705f29be6e3229a0d36f4818559fde57685a34
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-06-17 22:07:34 -0600

    6.- strings

---
+++ strings.py
@@ -0,0 +1,18 @@
+print('100 + 50')
+
+print('Hola' + ' Gabo')
+
+print('Hola' + " " + 'Gabo')
+
+print('Hola' + " " + 'Gabo')
+
+print("Yo soy \"Gabo\"")
+
+print("Esta es una linea\nY esta es otra linea")
+print("Esta es la tercera linea")
+
+print("\tEsta es una tabulacion")
+
+print("Usando un apostrofe: don\'t")
+
+print("Usando una barra invertida: \\")
