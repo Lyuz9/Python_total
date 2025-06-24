51dfd49be550c4f3a805425599c518b920a92186
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-06-24 16:37:55 -0600

    9.- proyecto del dia de los temas 5 y 6

---
+++ proyecto_dia_1.py
@@ -0,0 +1,3 @@
+print("El nombre de la marca es: \n'"+ (input("Palabra atractiva o calificativa para el agrado del cliente; Ejemplo \"Monster\": ")) 
+      + " " + 
+      (input("Complemento agradable, eslogan o comentario de lo que ofrecera el producto; Ejemplo \"Lite\": ")) + "'")
