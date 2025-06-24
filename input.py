2b724448ca4aa139bc89e499680b9d0979e062a3
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-06-24 10:47:38 -0600

    6.- uso de input

---
+++ input.py
@@ -0,0 +1,13 @@
+print("Dime tu nombre: ")
+print("Dime tu apellido: ")
+
+#print(input("Dime tu nombre: "))
+#print(input("Dime tu apellido: "))
+
+#print("Tu nombre es "+input("Dime tu nombre: "))
+
+print("Tu nombre es " + input("Dime tu nombre: ") + " " + input("Dime tu apellido: "))
+
+# opcional para visualización:
+print("Tu nombre es " + (input("Dime tu nombre: ")) + " " + (input("Dime tu apellido: ")))
+# 
