3e64d27c29654dca12fcf00342b2b4d317c08853
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-06-25 15:06:04 -0600

    14.- variables

---
+++ variables.py
@@ -0,0 +1,38 @@
+nombre = "Gabo"
+print(nombre)
+
+nombre = "Gabriel"
+print(nombre)
+
+
+
+
+
+edad = 22
+edad2 = 25
+
+print(edad)
+print(edad + edad2)
+
+
+
+
+texto = input("dime tu nombre ")
+print("Tu nombre es: " + texto)
+
+
+
+
+nombre1 = "hola "
+nombre2 = "python"
+frase = nombre1 + nombre2
+print(frase)
+
+
+
+
+edad1 = 22
+edad2 = 25
+
+edades = edad1 + edad2
+print(edades)
