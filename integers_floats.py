8ee170b31648bf44ad24cf22a5aeb5ad7f5634ae
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-06-25 15:19:52 -0600

    17.- Intehers y floats

---
+++ integers_floats.py
@@ -0,0 +1,32 @@
+mi_numero = 1
+print(mi_numero)
+
+mi_numero = 1 + 3
+print(mi_numero + mi_numero)
+print(type(mi_numero))
+
+
+print("-----------------")
+
+
+mi_numero = 5.8
+print(mi_numero)
+
+print(mi_numero + mi_numero)
+print(type(mi_numero))
+
+
+print("-----------------")
+
+
+mi_numero = 5 + 5.8
+print(mi_numero)
+
+print(type(mi_numero))
+
+
+print("-----------------")
+
+
+edad = input("Ingresa tu edad: ")
+print("Tu edad es: " + edad)
