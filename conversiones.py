47639ca4da992320f2d024c9ec81499b8246f0ac
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-06-26 11:43:58 -0600

    17.- conversiones entre tipos de datos

---
+++ conversiones.py
@@ -0,0 +1,34 @@
+#num1 = 20
+#num2 = 30.5
+
+
+
+#num1 = num1 + num2
+
+
+
+#print(type(num1))
+#print(type(num2))
+
+num1 = 5.8
+print(num1)
+print(type(num1))
+
+
+
+num2 = int(num1)
+print(num2)
+print(type(num2))
+
+
+
+
+edad = input("Ingresa tu edad: ")
+
+print(type(edad))
+edad = int(edad)
+print(type(edad))
+
+
+nueva_edad = edad + 1
+print("Tu  nueva edad es: " + str(nueva_edad))
