8e8ba5065505d55ffefeb6b10d11da7602ab4e79
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-06-30 15:15:37 -0600

    20.- redondeo

---
+++ redondear.py
@@ -0,0 +1,16 @@
+# Redondear
+print(round(90/7))
+
+# modo distinto
+resultado = round(90/7)
+print(resultado)
+
+# redondear 2 decimales
+valor = round(95.66666666666666, 2)
+print(valor)
+print(type(valor))
+
+# redondear a entero
+valor = round(95.66666666666666)
+print(valor)
+print(type(valor))
