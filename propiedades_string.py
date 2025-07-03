a904e5ee6f44fcf7d97831ad1a6b32f9b225e37a
Lyuz9 <luisgabrielhernandezresendiz@gmail.com>
2025-07-03 16:10:34 -0600

    30.- Propiedades de String

---
+++ propiedades_string.py
@@ -0,0 +1,20 @@
+n1 = "Al"
+n2 = "ia"
+
+print(n1 + n2)
+
+
+
+poema = """Mil pequeños peces blancos
+como si hirviera el color del agua"""
+
+hola = "hola mundo"
+
+print(poema)
+print("agua" in poema)
+print("sol" not in poema)
+print("agua" not in poema)
+
+
+
+print(len(hola))
